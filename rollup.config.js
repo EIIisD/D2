b96eb8a19ed6fcfa7e1a5679d4cf573ccd13a403
Ellis Donovan <e.donovan@reply.com>
2020-07-03 22:50:54 +0100

    add p-- component

--- rollup.config.js
+++ rollup.config.js
@@ -1,54 +1,69 @@
+const { sync } = require('fast-glob');
+const { basename } = require('path');
+const { writeFileSync } = require('jsonfile');
+
+let components = sync('src/lib/*.svelte').map(file => {
+  file = basename(file,'.svelte');
+  return { name:file, tag:`${file.toLowerCase()}--` };
+});
+
+writeFileSync('library.json', {components:components});
+
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import svelteSVG from 'rollup-plugin-svelte-svg';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default [
-  {
-    input: [
-      'src/lib/One.svelte',
-    ],
+const customElements = components.map(component => {
+  return {
+    input: `src/lib/${component.name}.svelte`,
     output: {
       dir: 'public/lib',
       format: 'esm',
     },
     plugins: [
-      svelte({ customElement: true }),
-      resolve(),
-    ],
-  },
-  {
-    input: 'src/app.js',
-    output: {
-      sourcemap: true,
-      format: 'iife',
-      name: 'app',
-      file: 'public/build/bundle.js'
-    },
-    plugins: [
       svelte({
-        dev: true,
-        css: css => { css.write('public/build/bundle.css') }
+        customElement: true,
+        tag: component.tag,
       }),
-      svelteSVG(),
-      resolve({
-        browser: true,
-        dedupe: ['svelte'],
-      }),
-      commonjs(),
-      !production && serve(),
-      !production && livereload('public'),
-      production && terser()
-    ],
-    watch: {
-      clearScreen: false
-    }
+      resolve(),
+    ]
+  }
+});
+
+export default [{
+  input: 'src/app.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/build/bundle.js'
   },
-];
+  plugins: [
+    svelte({
+      dev: true,
+      css: css => { css.write('public/build/bundle.css') }
+    }),
+    json(),
+    svelteSVG(),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
+    !production && serve(),
+    !production && livereload('public'),
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}, ...customElements];
 
 function serve() {
 	let started = false;
